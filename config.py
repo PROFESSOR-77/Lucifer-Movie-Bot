3981a5b1bbca95a130f0fa8e5cfe29eae5cf5d0c
PROFESSOR-77 <109091765+PROFESSOR-77@users.noreply.github.com>
2022-07-16 22:27:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -28,7 +28,6 @@ ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ['CHANNELS'].split()]
 AUTH_GROUPS = [int(admin) for admin in environ.get("AUTH_GROUPS", "").split()]
 AUTH_USERS = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
-PROTECT_FILES = is_enabled((os.environ.get('PROTECT_FILES', "True")), True) 
 
 # ==================================
 # Empty 😂
