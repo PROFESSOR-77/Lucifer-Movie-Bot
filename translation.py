ebef4d66196377501fa1c592fdc8d6c5be023280
PROFESSOR-77 <109091765+PROFESSOR-77@users.noreply.github.com>
2022-07-16 19:50:28 +0530

    Add files via upload

---
+++ translation.py
@@ -0,0 +1,115 @@
+class LuciferMoringstar(object):
+
+    DEFAULT_MSG = """
+**Hello 👋 {mention}**,
+
+**I Am Not Only <a href="https://t.me/MoviesClub_Robot">Movie Club Bot</a> To Assist You But Also Employed At <a href="https://t.me/+ZLfYBUbS-adiY2E1">Movie Club</a> Group By <a href="https://t.me/helloheartbeatboss">HeartBeat</a> So You Can't Get My Service By Adding Me To Your Group So Don't Waste Your Time & Data 😉**
+
+**Better You Click Below & Join <a href="https://t.me/+ZLfYBUbS-adiY2E1">Movie Club</a> & Feel The Experience Of Downloading Unlimited Movies/ Series ✅**"""
+    
+    HELP_MSG = """**This help button contains only admin commands so only group admin can see help commands.**"""
+
+    DARKLORD_77 = """
+🤖 My Name : {bot_name}
+
+🤴 My Dev : HeartBeat
+
+🛠️ My Library : Pyrogram
+
+㊗️ My Language : Python 3
+
+🗄️ My Database : MongoDB 
+
+📡 My Server : Heroku"""
+
+    WHO_AM_I = """
+Hello 👋 {mention},
+I Can't Write Everything In My Bio So I Created This...
+
+╭──[─═ ᴘᴇʀꜱᴏɴᴀʟ ɪɴꜰᴏ ═─]
+├• Name⇝ Bikash Maity
+├• Gender⇝ Male
+├• TG Name⇝ <a href="https://t.me/helloheartbeatboss">HeartBeat</a> 
+├• Username⇝ @helloheartbeatboss
+├• Birthday⇝ 10th January 2006
+├• Age⇝ 16
+├• Relationship Status⇝ Single
+├• Nationality⇝ Indian
+├• Location⇝ West Bengal,India,Manikpur
+├• Contact Bot⇝ Coming Soon...
+╰──────⍟───────╯
+
+╭──[─═ ᴏᴡɴᴇʀ ᴀᴛ ═─]
+├•<a href="https://t.me/+ZLfYBUbS-adiY2E1">MOVIE CLUB 2.0</a>
+├•<a href="https://t.me/+pDzwoBleoxxhY2I1">MOVIE CLUB UPDATES</a>
+├•<a href="https://t.me/MoviesClub_Robot">MOVIE CLUB BOT 2.0</a>
+╰──────⍟───────╯
+
+Thank You For Reading Patiently 🙏"""
+    
+    FILE_CAPTIONS = """
+ ╭──[ミ★ MOVIE CLUB ★彡]──╮
+
+├• 👋 𝐇𝐞𝐥𝐥𝐨 {mention}
+
+├• ✅ 𝐘𝐨𝐮𝐫 𝐅𝐢𝐥𝐞 𝐈𝐬 𝐑𝐞𝐚𝐝𝐲
+
+├• 🎬 𝐓𝐢𝐭𝐥𝐞 : <code>{title}</code>
+
+├• 💾 𝐒𝐢𝐳𝐞 : {size}
+
+├• 🔘 𝐉𝐨𝐢𝐧 🎗 𝐒𝐡𝐚𝐫𝐞 🎗 𝐒𝐮𝐩𝐩𝐨𝐫𝐭
+
+├• 🔗 <a href="https://t.me/+ZLfYBUbS-adiY2E1">Movie Club 2.0</a>
+
+├• 📣 <a href="https://t.me/+pDzwoBleoxxhY2I1">Movie Club Updates</a>
+
+├• ⬆️ 𝐔𝐩𝐥𝐨𝐚𝐝𝐞𝐝 𝐁𝐲 <a href="https://t.me/helloheartbeatboss">HeartBeat</a>
+
+╰──────[ 👑 ]───────╯"""
+    
+    PR0FESS0R_99 = """
+**ADMIN COMMANDS**
+
+ » /broadcast - Reply Any Media Or Message
+ » /delete - Reply Files
+ » /deleteall - Delete All Files
+ » /total - How Many Files Saved
+ » /logger - Get Bot Logs
+ » /channel - Add Channel List"""
+
+    ADD_YOUR_GROUP = """**__ഞാൻ ജോലി ചെയ്യുന്ന ഗ്രൂപ്പിൽ ചേരൂ, നിങ്ങൾക്ക് ആവശ്യമുള്ള എല്ലാ സിനിമകളും ആ ഗ്രൂപ്പിൽ അഭ്യർത്ഥിക്കാം! ഇവിടെ അഭ്യർത്തിക്കരുത്..😤__**"""
+    SPELL_CHECK = """
+**Hello 👋{mention}**,
+
+**I Couldn't Find {query} You Asked For 🤷**
+
+**Click [GOOGLE] [IMDB] On Any Button And Find The Correct Movie/Series Name And Enter It Here ⤵️**
+
+**If You Do Not Receive The Movie/Series Even After Entering The Correct Name Then Your Requested Movie/Series Does Not Exit In My Database 🗄**"""
+
+    GET_MOVIE_1 = """
+**Hello 👋{mention}**,
+
+🎬 **Title** : [{title}]({url})
+📆 **Year** : {year}
+🌟 **Rating** : {rating}/10
+🎭 **Genres** : {genres}
+📤 Uploaded To : Movie Club Server
+🧑‍🔧 Get Support ✔️ HeartBeat
+
+📌 Press The Down Buttons To Access The File
+📌 This Post Will Be Deleted After 10 Minutes"""
+
+    GET_MOVIE_2 = """
+**Hello 👋{mention}**,
+
+🎬 **Title** : [{title}]({url})
+📆 **Year** : {year}
+🌟 **Rating** : {rating}/10
+🎭 **Genres** : {genres}
+📤 Uploaded To : Movie Club Server
+🧑‍🔧 Get Support ✔️ Heartbeat
+
+📌 Press The Down Buttons To Access The File
+📌 This Post Will Be Deleted After 10 Minutes"""
