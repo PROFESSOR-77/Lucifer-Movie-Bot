5cfd81ec39140a1f92bb4967a0323fbba990865e
PROFESSOR-77 <109091765+PROFESSOR-77@users.noreply.github.com>
2022-07-16 22:19:40 +0530

    Update callback.py

--- LuciferMovie_Bot/callback.py
+++ LuciferMovie_Bot/callback.py
@@ -292,7 +292,7 @@ async def cb_handler(client: LuciferMovie_Bot, query):
              InlineKeyboardButton("🏠 Home", callback_data="start"),
              InlineKeyboardButton("Close 🗑️", callback_data="close")
              ]]               
-            await query.message.edit(text=LuciferMovie.PROFESSOR-77.format(mention=query.from_user.mention, bot_name=bot_info.BOT_NAME, bot_username=bot_info.BOT_USERNAME, dev_name=DEV_NAME), reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
+            await query.message.edit(text=LuciferMovie.HeartBeat.format(mention=query.from_user.mention, bot_name=bot_info.BOT_NAME, bot_username=bot_info.BOT_USERNAME, dev_name=DEV_NAME), reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
 
         elif query.data == "close":
             await query.message.delete()
