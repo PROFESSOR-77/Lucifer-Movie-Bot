5d01558393c6fda5adbee3f47c2b8915b82139b3
Bikash Maity <109474243+BIKASH-99@users.noreply.github.com>
2022-07-17 21:16:03 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/PROFESSOR-77/LuciferMovie-Bot.git /LuciferMovie-Bot
+  git clone https://github.com/PROFESSOR-77/Lucifer-Movie-Bot.git /LuciferMovie-Bot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /LuciferMovie-Bot
+  git clone $UPSTREAM_REPO /Lucifer-Movie-Bot
 fi
-cd /LuciferMovie-Bot
+cd /Lucifer-Movie-Bot
 pip3 install -U -r requirements.txt
 echo "Starting Lucifer Movie Bot...."
 python3 main.py
